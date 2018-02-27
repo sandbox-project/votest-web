36fb2cb700fa0b0f3d5a7930ee618518aedaf216
MVolodya <medvid.volodya@gmail.com>
2018-02-27 23:29:31 +0200

    added BrowserRouter for index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './Components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './reset.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { BrowserRouter } from "react-router-dom";
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
