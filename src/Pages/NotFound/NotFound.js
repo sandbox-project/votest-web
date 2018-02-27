fede370e9e47d079f2b7e221bac83416ac8205b4
MVolodya <medvid.volodya@gmail.com>
2018-02-28 00:30:37 +0200

    added NotFound page

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="NotFound">
+        NotFound
+      </div>
+    );
+
+  }
+}
+
+export default NotFound;
